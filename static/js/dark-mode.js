5ee35dceb2be10cb1f463c7bd18f7858f3db5eb6
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2023-11-16 17:22:47 +0100

    Tweak dark mode script

--- static/js/dark-mode.js
+++ static/js/dark-mode.js
@@ -1,34 +1,56 @@
-tailwind.config = {
-  darkMode: "class",
-};
+let get = (id) => document.getElementById(id);
+let stored = (k) => localStorage.getItem(k);
+let stores = (k, v) => localStorage.setItem(k, v);
 
-var get = (id) => document.getElementById(id);
-var stored = (k) => localStorage.getItem(k);
-var stores = (k, v) => localStorage.setItem(k, v);
-var target = document.body;
-var themeToggleDarkIcon = get("theme-toggle-dark-icon");
-var themeToggleLightIcon = get("theme-toggle-light-icon");
+// Set tailwid darkmode to be driven off a classname
+if (tailwind) tailwind.config = { darkMode: "class" };
+if (isDarkMode()) document.body.classList.add("dark");
 
-// Change the icons inside the button based on previous settings
-var hasLocal = "color-theme" in localStorage;
-var localValue = hasLocal && stored("color-theme");
-var mediaDark = "(prefers-color-scheme: dark)";
-var isDark =
-  (hasLocal && localValue === "dark") ||
-  (!hasLocal && window.matchMedia(mediaDark).matches);
-if (isDark) {
-  themeToggleLightIcon.classList.remove("hidden");
-  target.classList.add("dark");
-} else {
-  themeToggleDarkIcon.classList.remove("hidden");
-  target.classList.remove("dark");
+function isDarkMode() {
+  let hasLocal = "color-theme" in localStorage;
+  let localValue = hasLocal && stored("color-theme");
+  let mediaDark = "(prefers-color-scheme: dark)";
+
+  // Try and determine the current prefference
+  if (hasLocal && localValue === "dark") return true;
+  if (!hasLocal && window.matchMedia(mediaDark).matches) return true;
+  return false;
 }
 
-var themeToggleBtn = get("theme-toggle");
-themeToggleBtn.addEventListener("click", function () {
+function initDarkMode() {
+  // Get refferences to dark mode toggle buttons
+  let target = document.body;
+  let themeToggleDarkIcon = get("theme-toggle-dark-icon");
+  let themeToggleLightIcon = get("theme-toggle-light-icon");
+
+  // Try and determine the current prefference
+  let isDark = isDarkMode();
+
+  // Change the icons inside the button based on previous settings
+  if (isDark) {
+    themeToggleLightIcon && themeToggleLightIcon.classList.remove("hidden");
+    target.classList.add("dark");
+  } else {
+    themeToggleDarkIcon && themeToggleDarkIcon.classList.remove("hidden");
+    target.classList.remove("dark");
+  }
+}
+
+function bindDarkModeButton() {
+  let themeToggleBtn = get("theme-toggle");
+  if (themeToggleBtn) {
+    themeToggleBtn.addEventListener("click", toggleDarkMode);
+  }
+}
+
+function toggleDarkMode() {
+  let target = document.body;
+  let themeToggleDarkIcon = get("theme-toggle-dark-icon");
+  let themeToggleLightIcon = get("theme-toggle-light-icon");
+
   // toggle icons inside button
-  themeToggleDarkIcon.classList.toggle("hidden");
-  themeToggleLightIcon.classList.toggle("hidden");
+  themeToggleDarkIcon && themeToggleDarkIcon.classList.toggle("hidden");
+  themeToggleLightIcon && themeToggleLightIcon.classList.toggle("hidden");
 
   // if set via local storage previously
   var value = stored("color-theme");
@@ -39,4 +61,10 @@ themeToggleBtn.addEventListener("click", function () {
     target.classList.remove("dark");
     stores("color-theme", "light");
   }
-});
+}
+
+// Bind dark mode as soon as possible
+addEventListener("DOMContentLoaded", (event) => initDarkMode());
+
+// Set dark mode toggle only when page has loaded
+addEventListener("load", (event) => bindDarkModeButton());
