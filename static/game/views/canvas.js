fe52a2052986243e6309f04a5e68bb072d49b636
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2023-11-22 12:43:03 +0100

    Add placeholders for missing views

---
+++ static/game/views/canvas.js
@@ -0,0 +1,15 @@
+class HtmlRenderer extends GameRendererCore {
+  constructor(target) {
+    super(target);
+  }
+
+  render(target) {}
+
+  setLoading(active, context) {}
+
+  createView(config, data) {}
+
+  updateView(config, data) {}
+
+  updateFPS(fps) {}
+}
---
+++ static/game/views/webgl.js
@@ -0,0 +1,16 @@
+class HtmlRenderer extends GameRendererCore {
+    constructor(target) {
+      super(target);
+    }
+  
+    render(target) {}
+  
+    setLoading(active, context) {}
+  
+    createView(config, data) {}
+  
+    updateView(config, data) {}
+  
+    updateFPS(fps) {}
+  }
+  
